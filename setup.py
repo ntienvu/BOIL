629b06e1567e1c6c3a12b9a64d18b6e6ff2d2859
Vu Nguyen <ntienvu@gmail.com>
2022-05-07 23:00:10 +0930

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,15 @@ setup(
     include_package_data = True,
     description='BOIL',
     install_requires=[
-        "numpy >= 1.9.0",
-        "scipy >= 0.14.0",
-        "scikit-learn >= 0.16.1",
+        "numpy >= 1.21.6",
+        "scipy == 1.4.1",
+        "scikit-learn >= 1.0.2",
         "tabulate>=0.8.7",
         "matplotlib>=3.1.0",
-        "tensorflow>=2.0",
-        "gym>=0.5"
+        "tensorflow==2.8.0",
+        "gym>=0.5",
+        "sobol-seq>=0.2.0",
+        "tensorflow-probability==0.16.0",
+        "tqdm>=4.64.0"
     ],
 )
