aac4c643c54c2ba52756209cd40ede3107a8a6c4
Vu Nguyen <ntienvu@gmail.com>
2023-12-22 10:53:02 +1030

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(
         "scikit-learn >= 1.0.2",
         "tabulate>=0.8.7",
         "matplotlib>=3.1.0",
-        "tensorflow==2.8.0",
+        "tensorflow>=2.8.0",
         "gym>=0.5",
         "sobol-seq>=0.2.0",
-        "tensorflow-probability==0.16.0",
+        "tensorflow-probability>=0.16.0",
         "tqdm>=4.64.0",
         "pygame>=2.1.0"
     ],
